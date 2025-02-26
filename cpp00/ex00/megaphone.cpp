033a5b187b6eededf5cd4eeba10664a6ab696264
rhanitra <rhanitra@student.42antananarivo.mg>
2025-02-26 19:27:24 +0300

    init

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,20 +6,38 @@
 /*   By: rhanitra <rhanitra@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 16:46:49 by rhanitra          #+#    #+#             */
-/*   Updated: 2025/02/25 19:22:06 by rhanitra         ###   ########.fr       */
+/*   Updated: 2025/02/26 17:33:34 by rhanitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 #include <cctype>
 
 int main(int argc, char **argv)
-{
-    
-    if (argc == 1 || (argc == 2 && !argv[0]))
+{ 
+    int     i;
+    int     j;
+    char    c;
+
+    i = 1;
+    c = 0;
+    if (argc == 1 || (argc == 2 && !argv[1][0]))
     {
         std::cout  << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
     }
-    
+    while (i < argc)
+    {
+        j = 0;
+        while (argv[i][j] != '\0')
+        {
+            c = toupper(argv[i][j]);
+            std::cout << c;
+            j++;
+        }
+        i++;
+    }
+    std::cout << std::endl;
     return (0);
 }
---
+++ cpp00/ex01/BrickWall.hpp
@@ -0,0 +1,13 @@
+#ifndef ADD_CONTACT_H
+# define ADD_CONTACT_H
+
+# include <iostream>
+
+class BrickWall
+{
+    public:
+        BrickWall(void);
+        ~BrickWall(void);
+};
+
+#endif
