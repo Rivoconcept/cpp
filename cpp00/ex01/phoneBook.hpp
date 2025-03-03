a0fb86369da199e21885fbb4aa0fdbb67b67df37
rhanitra <rhanitra@student.42antananarivo.mg>
2025-03-03 19:06:32 +0300

    init_ex01

---
+++ cpp00/ex01/phoneBook.hpp
@@ -0,0 +1,24 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <iostream>
+
+class phoneBook
+{
+private:
+   
+public:
+    phoneBook();
+    ~phoneBook();
+};
+
+phoneBook::phoneBook(/* args */)
+{
+}
+
+phoneBook::~phoneBook()
+{
+}
+
+
+#endif
