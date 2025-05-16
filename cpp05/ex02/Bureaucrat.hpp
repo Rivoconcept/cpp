206f2227bb647ba2259f1acb4152516a6dd5cf26
Rivo Vonjiniaina Hanitrarivelo <rhanitra@c1r10s5.42antananarivo.mg>
2025-05-16 18:28:19 +0300

    boost

---
+++ cpp05/ex02/Bureaucrat.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhanitra <rhanitra@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 17:47:29 by rhanitra          #+#    #+#             */
+/*   Updated: 2025/05/15 18:36:00 by rhanitra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Form;
+
+class Bureaucrat
+{
+    private:
+        const std::string _name;
+        int _grade;
+    
+    public:
+        Bureaucrat();
+        Bureaucrat(const std::string& name, int grade);
+        Bureaucrat(const Bureaucrat& bureaucrat);
+        Bureaucrat& operator=(const Bureaucrat& bureaucrat);
+        ~Bureaucrat();
+
+        const std::string& getName() const;
+        int getGrade() const;
+
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+
+        void incrementGrade();
+        void decrementGrade();
+
+        void signForm(Form& form) const
+;
+        
+};
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat);
+
+#endif
---
+++ cpp05/ex02/Form.hpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhanitra <rhanitra@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 12:37:34 by rhanitra          #+#    #+#             */
+/*   Updated: 2025/05/15 18:12:27 by rhanitra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat;
+
+class Form
+{
+    private:
+        const std::string _name;
+        bool _formSigned;
+        const int _gradeToSign;
+        const int _gradeToExecute;
+    
+    public:        
+        Form();
+        Form(const std::string name, int gts, int gte);
+        Form(const Form& other);
+        Form& operator=(const Form& other);
+        ~Form();
+
+        const std::string& getName() const;
+        bool getFormSigned() const;
+        int getGradeToSign() const;
+        int getGradeToExecute() const;
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char* what() const throw();
+        };
+
+        void beSigned(const Bureaucrat& bureaucrat);
+};
+
+std::ostream& operator<<(std::ostream& out, const Form& form);
+
+#endif
