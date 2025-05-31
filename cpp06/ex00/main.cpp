03f5a22be7078be736a5366330b418e8805824ef
Rivo Vonjiniaina Hanitrarivelo <rhanitra@c1r11s1.42antananarivo.mg>
2025-05-31 14:55:20 +0300

    sujet

---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhanitra <rhanitra@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 18:33:19 by rhanitra          #+#    #+#             */
+/*   Updated: 2025/05/26 17:47:53 by rhanitra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    try {
+        Bureaucrat a("Alice", 2);
+        std::cout << a << std::endl;
+
+        a.incrementGrade();
+        std::cout << a << std::endl;
+
+        a.incrementGrade();
+
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+
+    try
+    {
+        Bureaucrat b("Bob", 151);
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
