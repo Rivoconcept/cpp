19baddf751675019d184130eaaca9291188450a7
Rivoconcept <rivo.hanitrarivelo@gmail.com>
2025-06-27 16:19:23 +0300

    find

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rhanitra <rhanitra@student.42antananari    +#+  +:+       +#+        */
+/*   By: rivoinfo <rivoinfo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 14:53:00 by rhanitra          #+#    #+#             */
-/*   Updated: 2025/06/25 14:58:39 by rhanitra         ###   ########.fr       */
+/*   Updated: 2025/06/27 16:18:27 by rivoinfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,25 +47,28 @@ std::vector<int> ft_split_to_ints(const std::string& str, char delimiter)
 
 int main(int argc, char **argv)
 {
-    if (argc < 2 || (argc == 2 && !argv[1][0]))
+    if (argc < 3 || (argc == 2 && !argv[1][0]))
     {
-        std::cout << "Use: ./easyfind <more arguments separated by spaces>" << std::endl;
+        std::cout << "Use at least 3 parameters: ./easyfind <more arguments separated by spaces>" << std::endl;
         return 1;
     }
 
     std::string str;
-    for (int i = 1; i < argc; ++i)
+    for (int i = 1; i < argc - 1; ++i)
     {
         str += argv[i];
         if (i != argc - 1)
             str += " ";
     }
 
+
+
     std::vector<int> tokens = ft_split_to_ints(str, ' ');
     
+    std::size_t pos;
     try
     {
-        std::vector<int>::iterator it = easyfind(tokens, 3);
+        std::vector<int>::iterator it = easyfind(tokens, std::strtod(static_cast<std::string>(argv[argc - 1]), &pos));
         std::cout << "Found: " << *it << " at position " << (it - tokens.begin()) << std::endl;
     }
     catch (const std::exception& e)
