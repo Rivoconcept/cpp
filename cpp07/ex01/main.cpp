782c6d66acb08c4dc789c02f2134d6a973f53597
Rivoconcept <rivo.hanitrarivelo@gmail.com>
2025-06-27 21:22:51 +0300

    N

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -24,7 +24,7 @@ void printString(std::string val)
 
 int main(void)
 {
-    { 
+    {
         int tabInt[] = {5, 35, 87, 42, 9};
         size_t tabSize = sizeof(tabInt) / sizeof(tabInt[0]);
 
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -10,21 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+#ifndef SPAN_HPP
+#define SPAN_HPP
 
 #include <algorithm>
 #include <stdexcept>
 
-template <typename T>
-typename T::iterator easyfind(T& container, int value)
+class Span
 {
-    typename T::iterator it = std::find(container.begin(), container.end(), value);
-    if (it == container.end())
-    {
-        throw std::runtime_error("Value not found in container");
-    }
-    return (it);
-}
+
+};
 
 #endif
