bdf2e30ca720da84914df8d9f3e482cd987df7d2
Rivoconcept <rivo.hanitrarivelo@gmail.com>
2025-07-07 16:23:00 +0300

    pull

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rhanitra <rhanitra@student.42antananari    +#+  +:+       +#+        */
+/*   By: rivoinfo <rivoinfo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 17:19:19 by rhanitra          #+#    #+#             */
-/*   Updated: 2025/07/06 18:01:46 by rhanitra         ###   ########.fr       */
+/*   Updated: 2025/07/07 14:03:04 by rivoinfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
-
-
 
 BitcoinExchange::BitcoinExchange() : _fileContent(0), _dataBase(0) {}
 
@@ -42,6 +40,60 @@ const std::deque<std::string>& BitcoinExchange::getFileContent() const
     return (_fileContent);
 }
 
+bool BitcoinExchange::isLeapYear(int year) 
+{
+    if (year % 400 == 0)
+        return true;
+    if (year % 100 == 0)
+        return false;
+    if (year % 4 == 0)
+        return true;
+    return false;
+}
+
+bool BitcoinExchange::is_valid_date(int day, int month, int year)
+{
+    if (year < 1 || month < 1 || month > 12 || day < 1)
+        return false;
+
+    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    if (month == 2 && isLeapYear(year))
+        days_in_month[1] = 29;
+
+    if (day > days_in_month[month - 1])
+        return false;
+
+    return true;
+}
+
+std::vector<int> BitcoinExchange::ft_split_to_ints(const std::string& str, char delimiter)
+{
+    std::vector<int> output;
+    std::istringstream iss(str);
+    std::string token;
+    
+    while (std::getline(iss, token, delimiter))
+    {
+        if (!token.empty())
+        {
+            std::istringstream iss_token(token);
+            long n;
+            char extra;
+            
+            if (!(iss_token >> n) || (iss_token >> extra))
+                throw std::runtime_error("Token invalide: '" + token + "'");
+            
+            if (n < std::numeric_limits<int>::min() || n > std::numeric_limits<int>::max())
+                throw std::runtime_error("Nombre hors limites: " + token);
+            
+            output.push_back(static_cast<int>(n));
+        }
+    }
+    
+    return (output);
+}
+
 void BitcoinExchange::putDataBase(const std::string& inputFile)
 {
     std::ifstream ifs(inputFile.c_str());
@@ -68,10 +120,26 @@ void BitcoinExchange::putFileContent(const std::string& file)
     }
     
     std::string line;
+    std::deque<int> tabLine;
     
     while (std::getline(ifs, line))
     {
-        _fileContent.push_back(line);
+        line = std::regex_replace(line, std::regex("[-,]"), " ");
+        tabLine = ft_split_to_ints(line, ' ');
     }
     ifs.close();
-} 
+}
+
+void BitcoinExchange::findValue(const std::deque<std::string> dataFile)
+{
+    if (dataFile.empty())
+        return ;
+    
+    int d, m, y;
+
+    for (std::deque<std::string>::iterator it; dataFile.begin() != dataFile.end(); ++it)
+    {
+        
+    }
+
+}
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rhanitra <rhanitra@student.42antananari    +#+  +:+       +#+        */
+/*   By: rivoinfo <rivoinfo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 17:13:38 by rhanitra          #+#    #+#             */
-/*   Updated: 2025/07/06 17:59:27 by rhanitra         ###   ########.fr       */
+/*   Updated: 2025/07/07 13:57:08 by rivoinfo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,20 @@
 # include <string>
 #include <sstream>
 # include <fstream>
+#include <limits> 
+#include <regex>
 #include <deque>
 
 class BitcoinExchange
 {
     private:
+        std::deque<std::string> _dataToFind;
         std::deque<std::string> _fileContent;
         std::deque<std::string> _dataBase;
+
+        static bool isLeapYear(int year);
+        static bool is_valid_date(int day, int month, int year);
+        static std::deque<int> ft_split_to_string(const std::string& str, char delimiter);
 
     public:
 
@@ -39,7 +46,7 @@ class BitcoinExchange
         void putFileContent(const std::string& file);
         void putDataBase(const std::string& inputFile);
         
-        void formatValue(const std::string& value);
+        void findValue(const std::deque<std::string> dataFile);
         
 };
 
